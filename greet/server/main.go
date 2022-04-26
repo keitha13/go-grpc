1e72482782cef528aeded3054ac285aeb371553f
keitha13 <keitha9452@gmail.com>
2022-04-26 15:41:17 +0900

    first commit

---
+++ greet/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	pb "go_grpc/greet/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiseServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+	log.Printf("Listening on %s\n", addr)
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
